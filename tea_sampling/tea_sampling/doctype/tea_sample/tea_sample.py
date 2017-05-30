a02872a51d3e75d0d814544b94a4be3c5c3000b9
user <nivedita12011990@gmail.com>
2017-05-30 16:09:49 +0530

    name ser issue solved

--- tea_sampling/tea_sampling/doctype/tea_sample/tea_sample.py
+++ tea_sampling/tea_sampling/doctype/tea_sample/tea_sample.py
@@ -5,6 +5,14 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-
+from datetime import datetime,timedelta
+from frappe.utils import getdate, add_days, add_years, cstr,formatdate, strip
+from erpnext.stock.stock_ledger import get_previous_sle, NegativeStockError
+from frappe.model.naming import make_autoname
+from frappe import _
+from frappe.model.mapper import get_mapped_doc
+from frappe import utils
 class TeaSample(Document):
-	pass
+	def autoname(self):
+		#estateabr = frappe.db.get_value("Estate",{"estate_name": self.estatename}, "abbreviation")
+		self.name = make_autoname("SAM"+ "/" +self.customer_name+ "/.####")
